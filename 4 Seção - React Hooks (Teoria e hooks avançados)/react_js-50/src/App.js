062058a562a5034e75a52afce4bb101aa15e89f7
Daniel Oliveira <danielsapup3@gmail.com>
2023-10-29 09:34:24 -0400

    useCallback - React Hook

--- 4 Seção - React Hooks (Teoria e hooks avançados)/react_js-50/src/App.js
+++ 4 Seção - React Hooks (Teoria e hooks avançados)/react_js-50/src/App.js
@@ -1,38 +1,43 @@
+import p from 'prop-types';
 import './App.css';
-import { useState } from 'react';
+import React, { useCallback, useState } from 'react';
+
+// Compnt em outro ficheiro
+const Button = React.memo(function Button({ incrementButton }) {
+  console.log('Filho, renderizou');
+  return <button onClick={() => incrementButton(100)}>+</button>
+});
+
+Button.prototype = {
+  incrementButton: p.func,
+};
+//...
 
 
 /*------- Component -------*/
 function App() {
-
   const [counter, setCounter] = useState(0);
+
+  // const incrementCounter = useCallback(
+  //   (num) => {
+  //     setCounter(counter + num);
+  //   },
+  //   [counter],
+  // );
+
+  const incrementCounter = useCallback((num) => {
+    setCounter((c) => c + num);
+  }, []);
+
+  console.log('Pai, renderizou');
 
   return (
     <div className="App">
-
       <h1>Contador: {counter}</h1>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
-
+      <Button incrementButton={incrementCounter} />
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
